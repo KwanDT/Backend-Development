43aa11ebe4c3379cf3edf540bf653c7a793b1912
KwanDT <111258541+KwanDT@users.noreply.github.com>
2022-10-25 16:26:05 +0700

    Update quora.js

--- apps/quora.js
+++ apps/quora.js
@@ -45,7 +45,7 @@ quoraRouter.put("/:questionId", async (req, res) => {
   });
 });
 
-movieRouter.delete("/:questionId", async (req, res) => {
+quoraRouter.delete("/:questionId", async (req, res) => {
   const collection = db.collection("questions");
 
   const questionId = ObjectId(req.params.questionId);
