a340855d5d268c6ddf09155ef916ca3035439cd6
KwanDT <111258541+KwanDT@users.noreply.github.com>
2022-10-28 17:34:49 +0700

    Delete db.js

--- server/utils/db.js
+++
@@ -1,9 +0,0 @@
-import * as pg from "pg";
-const { Pool } = pg.default;
-
-const pool = new Pool({
-  connectionString:
-    "postgresql://postgres@localhost:5432/skill-checkpoint-2",
-});
-
-export { pool };
